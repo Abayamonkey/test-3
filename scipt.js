c4eb9a2f023a784ea81e632dd4cdc037f7799d0b
Abayamonkey <145524013+Abayamonkey@users.noreply.github.com>
2024-05-08 04:59:57 +0000

    e

---
+++ scipt.js
@@ -0,0 +1,10 @@
+const rentForm = document.getElementById('rent-form');
+const carSelect = document.getElementById('car-select');
+
+// Add options for carSelect
+// Add event listener for form submission
+const lessonForm = document.getElementById('lesson-form');
+const lessonSelect = document.getElementById('lesson-select');
+
+// Add options for lessonSelect
+// Add event listener for form submission
